9f2375f551bd6866afaf4d88e3846896a08ecbaf
dart94 <167586944+dart94@users.noreply.github.com>
2024-11-08 15:19:45 -0700

    actualizacion de variables

--- includes/config/databe.php
+++ includes/config/databe.php
@@ -3,17 +3,16 @@
 function conectarDB(): mysqli
 {
     $db = new mysqli(
-        $_ENV['DB_HOST'],
-        $_ENV['DB_USER'],
-        $_ENV['DB_PASS'],
-        $_ENV['DB_NAME']
+        getenv('DB_HOST'),
+        getenv('DB_USER'),
+        getenv('DB_PASS'),
+        getenv('DB_NAME')
     );
 
     $db->set_charset('utf8');
 
-    if (!$db) {
-        echo "No se pudo conectar";
-        exit;
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
     }
 
     return $db;
