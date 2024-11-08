e4083a9c516b50761d1c64a2f421e7add6fcf8b5
dart94 <167586944+dart94@users.noreply.github.com>
2024-11-08 15:34:55 -0700

    comentario

--- includes/config/databe.php
+++ includes/config/databe.php
@@ -1,5 +1,9 @@
-<!-- Comentario -->
 <?php
+
+echo 'DB_HOST: ' . getenv('DB_HOST') . "<br>";
+echo 'DB_USER: ' . getenv('DB_USER') . "<br>";
+echo 'DB_PASS: ' . getenv('DB_PASS') . "<br>";
+echo 'DB_NAME: ' . getenv('DB_NAME') . "<br>";
 
 function conectarDB(): mysqli
 {
