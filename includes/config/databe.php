d746b912cab3ebd1c7ac4c67a842b10a6ffecfa0
dart94 <167586944+dart94@users.noreply.github.com>
2024-11-09 12:36:31 -0700

    ok

--- includes/config/databe.php
+++ includes/config/databe.php
@@ -6,7 +6,7 @@ function conectarDB(): mysqli
     $user = getenv('MYSQLUSER') ?: die("Error: La variable MYSQLUSER no está definida.");
     $pass = getenv('MYSQLPASSWORD') ?: die("Error: La variable MYSQLPASSWORD no está definida.");
     $name = getenv('MYSQLDATABASE') ?: die("Error: La variable MYSQLDATABASE no está definida.");
-    $port = getenv('MYSQLPORT') ?: 3306;
+    $port = 3306;
 
     // Intenta conectar a la base de datos
     $db = new mysqli($host, $user, $pass, $name, $port);
