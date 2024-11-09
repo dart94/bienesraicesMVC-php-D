2ad38c019414e3b888c76294ea99f1a5f84222be
dart94 <167586944+dart94@users.noreply.github.com>
2024-11-09 09:21:03 -0700

    prueba para railway

--- includes/config/databe.php
+++ includes/config/databe.php
@@ -2,20 +2,19 @@
 
 function conectarDB(): mysqli
 {
-    $db = new mysqli(
-        $_ENV['DB_HOST'],
-        $_ENV['DB_USER'],
-        $_ENV['DB_PASS'],
-        $_ENV['DB_NAME'],
-        $_ENV['DB_PORT']
-    );
+    $host = getenv('DB_HOST');
+    $user = getenv('DB_USER');
+    $pass = getenv('DB_PASS');
+    $name = getenv('DB_NAME');
+    $port = getenv('DB_PORT');
 
-    $db->set_charset('utf8');
+    $db = new mysqli($host, $user, $pass, $name, $port);
 
-    if (!$db) {
-        echo "No se pudo conectar";
+    if ($db->connect_error) {
+        echo "No se pudo conectar: " . $db->connect_error;
         exit;
     }
 
+    $db->set_charset('utf8');
     return $db;
 }
