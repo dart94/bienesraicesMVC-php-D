b8326d7f19b95a6c320ba5e4fbcfe690a21a1e93
dart94 <167586944+dart94@users.noreply.github.com>
2024-11-08 18:18:08 -0700

    datab

--- includes/config/databe.php
+++ includes/config/databe.php
@@ -1,5 +1,17 @@
+// Conexión a la base de datos
 <?php
 function conectarDB() : mysqli {
+
+        // Debugging
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+        
+        echo "Intentando conectar con los siguientes parámetros:<br>";
+        echo "Host: " . $_ENV['MYSQLHOST'] . "<br>";
+        echo "User: " . $_ENV['MYSQLUSER'] . "<br>";
+        echo "Database: " . $_ENV['MYSQLDATABASE'] . "<br>";
+        echo "Port: " . ($_ENV['MYSQLPORT'] ?? '3306') . "<br>";
+        echo "<br>";
     $db = new mysqli(
         $_ENV['MYSQLHOST'] ?? '',
         $_ENV['MYSQLUSER'] ?? '',
