d5e075ceec4b2243322227bcf0b02bb2244efa0d
dart94 <167586944+dart94@users.noreply.github.com>
2024-11-09 12:41:14 -0700

    ok

--- includes/config/databe.php
+++ includes/config/databe.php
@@ -2,13 +2,13 @@
 
 function conectarDB(): mysqli
 {
-    $host = getenv('RAILWAY_PRIVATE_DOMAIN') ?: die("Error: La variable RAILWAY_PRIVATE_DOMAIN no está definida.");
+    // Usa el host y puerto externos proporcionados en MYSQL_URL
+    $host = 'autorack.proxy.rlwy.net';
+    $port = 48982;
     $user = getenv('MYSQLUSER') ?: die("Error: La variable MYSQLUSER no está definida.");
     $pass = getenv('MYSQLPASSWORD') ?: die("Error: La variable MYSQLPASSWORD no está definida.");
     $name = getenv('MYSQLDATABASE') ?: die("Error: La variable MYSQLDATABASE no está definida.");
-    $port = 3306;
 
-    // Intenta conectar a la base de datos
     $db = new mysqli($host, $user, $pass, $name, $port);
 
     if ($db->connect_error) {
